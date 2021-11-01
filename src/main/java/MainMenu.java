9e5bbad5a758623d7fb838a1fb63dd2552d6173f
Alexander <74402359+alexanderwalford-official@users.noreply.github.com>
2021-11-01 14:21:20 +0000

    Main Menu (basic)
    Basic GUI built for main menu, started working on the audio engine.

--- src/main/java/MainMenu.java
+++ src/main/java/MainMenu.java
@@ -9,31 +9,84 @@ public class MainMenu {
         frame.pack();
         frame.setSize(1280, 720); // set the width and height of the window
         frame.setTitle("Survival Craft"); // set the window title
-        frame.setIconImage(new ImageIcon("src/main/src/player.jpg").getImage()); // set the window icon
-
+        frame.setIconImage(new ImageIcon("src/main/src/graphics/player.jpg").getImage()); // set the window icon
+        frame.setBackground(Color.white);
+        frame.setResizable(false);
         frame.setLocationRelativeTo(null); // window center screen
 
-        JButton button = new JButton("Button 1 (PAGE_START)");
-        frame.add(button, BorderLayout.PAGE_START);
+        // adding elements to the JFrame
 
-        button = new JButton("Button 2 (CENTER)");
-        button.setPreferredSize(new Dimension(200, 100));
-        frame.add(button, BorderLayout.CENTER);
+        // top spacing
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(200, 100));
+        panel.setBackground(Color.white);
+        frame.add(panel, BorderLayout.PAGE_START);
 
-        button = new JButton("Button 3 (LINE_START)");
-        frame.add(button, BorderLayout.LINE_START);
+        JPanel panel3 = new JPanel();
+        panel3.setPreferredSize(new Dimension(200, 40));
+        panel3.setBackground(Color.white);
 
-        button = new JButton("Long-Named Button 4 (PAGE_END)");
-        frame.add(button, BorderLayout.PAGE_END);
+        // game logo
+        JLabel gamelogo = new JLabel(new ImageIcon("src/main/src/graphics/gamelogo-long.jpg"));
+        gamelogo.setPreferredSize(new Dimension(1280,150));
+        panel3.add(gamelogo);
 
-        button = new JButton("5 (LINE_END)");
-        frame.add(button, BorderLayout.LINE_END);
+        // buttons
+        JButton singleplayer = new JButton("Single Player");
+        singleplayer.setPreferredSize(new Dimension(200, 100));
+        singleplayer.setFocusPainted(false);
+        singleplayer.setContentAreaFilled(false);
+        panel3.add(singleplayer);
+
+        JButton multiplayer = new JButton("Multi Player");
+        multiplayer.setPreferredSize(new Dimension(200, 100));
+        multiplayer.setFocusPainted(false);
+        multiplayer.setContentAreaFilled(false);
+        panel3.add(multiplayer);
+
+        JButton leaderboard = new JButton("Leader Board");
+        leaderboard.setPreferredSize(new Dimension(200, 100));
+        leaderboard.setFocusPainted(false);
+        leaderboard.setContentAreaFilled(false);
+        panel3.add(leaderboard);
+
+        JButton settings = new JButton("Game Settings");
+        settings.setPreferredSize(new Dimension(200, 100));
+        settings.setFocusPainted(false);
+        settings.setContentAreaFilled(false);
+        panel3.add(settings);
+
+        frame.add(panel3, BorderLayout.CENTER);
+
+        JPanel panel4 = new JPanel();
+        panel4.setPreferredSize(new Dimension(200, 40));
+        panel4.setBackground(Color.white);
+        frame.add(panel4, BorderLayout.LINE_START);
+
+        JPanel panel2 = new JPanel();
+        panel2.setBackground(Color.white);
+        panel2.setPreferredSize(new Dimension(200, 40));
+        JLabel footertext = new JLabel("Version 0.1 ALPHA by Alexander Walford");
+        footertext.setFont(new Font("Srif", Font.PLAIN, 20));
+        panel2.add(footertext);
+        frame.add(panel2, BorderLayout.PAGE_END);
+
+        JPanel panel6= new JPanel();
+        panel6.setBackground(Color.white);
+        panel6.setPreferredSize(new Dimension(200, 40));
+        frame.add(panel6, BorderLayout.LINE_END);
 
         frame.setVisible(true); // show the window
+
+        // start the background music from the AudioEngine
+        AudioEngine audio = new AudioEngine();
+        audio.HandleBackgroundMusic();
     }
 
+    // probably not going to implement in main menu
     public static void update () {
         // called once per frame
+
     }
 
 }
