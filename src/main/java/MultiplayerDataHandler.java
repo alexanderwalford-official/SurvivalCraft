a3065fd9593420871c89bcf302d84236b190953a
Alexander <74402359+alexanderwalford-official@users.noreply.github.com>
2021-11-06 15:16:12 +0000

    Added MultiplayerInput class file, need moficiation

--- src/main/java/MultiplayerDataHandler.java
+++ src/main/java/MultiplayerDataHandler.java
@@ -21,7 +21,8 @@ public class MultiplayerDataHandler {
     // call GetData(username) to get player details from the server
     // call HandleData(HandleData) to set the player's position from the data
 
-    public static void SendData () throws IOException {
+
+    public static byte[] SetPlayer (String apikey, String username, String lobby, String position, String rotation) throws IOException {
         URL url = new URL("https://renovatesoftware.com/API/setplayerdata/");
         URLConnection con = url.openConnection();
         HttpURLConnection http = (HttpURLConnection)con;
@@ -30,11 +31,11 @@ public class MultiplayerDataHandler {
 
         Map<String,String> arguments = new HashMap<>();
 
-        arguments.put("apikey", "550039706949"); // provide the API key
-        arguments.put("playerid", "player3453"); // set the player id
-        arguments.put("lobbyid", "lobby3534"); // set the lobby id
-        arguments.put("position", "0,0,0"); // set the player position
-        arguments.put("rotation", "0,0,0"); // set the player rotation
+        arguments.put("apikey", apikey); // provide the API key
+        arguments.put("playerid", username); // set the player id
+        arguments.put("lobbyid", lobby); // set the lobby id
+        arguments.put("position", position); // set the player position
+        arguments.put("rotation", rotation); // set the player rotation
 
         StringJoiner sj = new StringJoiner("&");
         for(Map.Entry<String,String> entry : arguments.entrySet())
@@ -48,16 +49,64 @@ public class MultiplayerDataHandler {
         http.connect();
         try(OutputStream os = http.getOutputStream()) {
             // handle output
-            os.write(out);
+            return out;
         }
     }
 
-    public static void GetData (String playername) {
+    public static byte[] GetPlayer (String apikey, String username) throws IOException {
+        URL url = new URL("https://renovatesoftware.com/API/getplayerdata/");
+        URLConnection con = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection)con;
+        http.setRequestMethod("POST"); // set the method to POST
+        http.setDoOutput(true);
 
+        Map<String,String> arguments = new HashMap<>();
+
+        arguments.put("apikey", apikey); // provide the API key
+        arguments.put("playerid", username); // set the player id
+
+        StringJoiner sj = new StringJoiner("&");
+        for(Map.Entry<String,String> entry : arguments.entrySet())
+            sj.add(URLEncoder.encode(entry.getKey(), "UTF-8") + "="
+                    + URLEncoder.encode(entry.getValue(), "UTF-8"));
+        byte[] out = sj.toString().getBytes(StandardCharsets.UTF_8);
+        int length = out.length;
+
+        http.setFixedLengthStreamingMode(length);
+        http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        http.connect();
+        try(OutputStream os = http.getOutputStream()) {
+            // handle output
+            return out;
+        }
     }
 
-    public static void HandleData (String playername, String playerpos, String playerrot) {
+    public static byte[] GetPlayerList (String apikey, String lobby) throws IOException {
+        URL url = new URL("https://renovatesoftware.com/API/getplayerlist/");
+        URLConnection con = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection)con;
+        http.setRequestMethod("POST"); // set the method to POST
+        http.setDoOutput(true);
 
+        Map<String,String> arguments = new HashMap<>();
+
+        arguments.put("apikey", apikey); // provide the API key
+        arguments.put("lobbyid", lobby); // set the lobby id
+
+        StringJoiner sj = new StringJoiner("&");
+        for(Map.Entry<String,String> entry : arguments.entrySet())
+            sj.add(URLEncoder.encode(entry.getKey(), "UTF-8") + "="
+                    + URLEncoder.encode(entry.getValue(), "UTF-8"));
+        byte[] out = sj.toString().getBytes(StandardCharsets.UTF_8);
+        int length = out.length;
+
+        http.setFixedLengthStreamingMode(length);
+        http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        http.connect();
+        try(OutputStream os = http.getOutputStream()) {
+            // handle output
+            return out;
+        }
     }
 
 }
---
+++ src/main/java/MultiplayerInput.java
@@ -0,0 +1,89 @@
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.KeyEvent;
+
+public class MultiplayerInput {
+    private static volatile boolean wPressed = false;
+    private static volatile boolean aPressed = false;
+    private static volatile boolean sPressed = false;
+    private static volatile boolean dPressed = false;
+    public static int movespeed = 4;
+
+    public static boolean isWPressed() {
+        synchronized (PlayerInput.class) {
+            return wPressed;
+        }
+    }
+
+    public static boolean isAPressed() {
+        synchronized (PlayerInput.class) {
+            return aPressed;
+        }
+    }
+
+    public static boolean isSPressed() {
+        synchronized (PlayerInput.class) {
+            return sPressed;
+        }
+    }
+
+    public static boolean isDPressed() {
+        synchronized (PlayerInput.class) {
+            return dPressed;
+        }
+    }
+
+    public static void main() {
+        KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
+            @Override
+            public boolean dispatchKeyEvent(KeyEvent ke) {
+                synchronized (PlayerInput.class) {
+                    switch (ke.getID()) {
+                        case KeyEvent.KEY_PRESSED:
+                            // W
+                            if (ke.getKeyCode() == KeyEvent.VK_W && RenderSinglePlayerMap.player.getLocation().y > 0) {
+                                wPressed = true;
+                                RenderSinglePlayerMap.player.setLocation(RenderSinglePlayerMap.player.getLocation().x,RenderSinglePlayerMap.player.getLocation().y-movespeed);
+                            }
+                            // A
+                            else if (ke.getKeyCode() == KeyEvent.VK_A && RenderSinglePlayerMap.player.getLocation().x > 0) {
+                                aPressed = true;
+                                RenderSinglePlayerMap.player.setLocation(RenderSinglePlayerMap.player.getLocation().x-movespeed,RenderSinglePlayerMap.player.getLocation().y);
+                            }
+                            // S
+                            else if (ke.getKeyCode() == KeyEvent.VK_S && RenderSinglePlayerMap.player.getLocation().y < RenderSinglePlayerMap.frame.getHeight() - RenderSinglePlayerMap.playerheight * 1.5) { // stops the player object from going out of bounds
+                                sPressed = true;
+                                RenderSinglePlayerMap.player.setLocation(RenderSinglePlayerMap.player.getLocation().x,RenderSinglePlayerMap.player.getLocation().y+movespeed); // sets the position of the player object
+                            }
+                            // D
+                            else if (ke.getKeyCode() == KeyEvent.VK_D && RenderSinglePlayerMap.player.getLocation().x < RenderSinglePlayerMap.frame.getWidth() - RenderSinglePlayerMap.playerwidth * 1.5) {
+                                dPressed = true;
+                                RenderSinglePlayerMap.player.setLocation(RenderSinglePlayerMap.player.getLocation().x+movespeed,RenderSinglePlayerMap.player.getLocation().y);
+                            }
+                            break;
+
+                        case KeyEvent.KEY_RELEASED:
+                            // W
+                            if (ke.getKeyCode() == KeyEvent.VK_W) {
+                                wPressed = false;
+                            }
+                            // A
+                            else if (ke.getKeyCode() == KeyEvent.VK_A) {
+                                aPressed = false;
+                            }
+                            // S
+                            else if (ke.getKeyCode() == KeyEvent.VK_S) {
+                                sPressed = false;
+                            }
+                            // D
+                            else if (ke.getKeyCode() == KeyEvent.VK_D) {
+                                dPressed = false;
+                            }
+                            break;
+                    }
+                    return false;
+                }
+            }
+        });
+    }
+}
